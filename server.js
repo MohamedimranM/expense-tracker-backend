d48e28e0c145040c67f0f675e76379b13ff60c73
MohamedimranM <mohamedimranm04@gmail.com>
2026-06-20 18:57:26 +0530

    scanned

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/expenses', require('./routes/expenses'));
 app.use('/api/dashboard', require('./routes/dashboard'));
 app.use('/api/reports', require('./routes/reports'));
-app.use('/api/bills', require('./routes/bills'));
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
